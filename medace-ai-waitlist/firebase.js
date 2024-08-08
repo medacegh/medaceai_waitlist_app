fa2c591beecf74a446b5f80855f7b086c71d8a35
Junaid Khan <38576660+jkhan2211@users.noreply.github.com>
2024-08-07 23:48:26 -0400

    fixed waitlist

--- medace-ai-waitlist/firebase.js
+++ medace-ai-waitlist/firebase.js
@@ -1,25 +1,22 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore"; // Corrected import
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBQTqCLxHjYUn9xvKoM4NdXfL7USxq5iCY",
-  authDomain: "medace-waitlist-app.firebaseapp.com",
-  projectId: "medace-waitlist-app",
-  storageBucket: "medace-waitlist-app.appspot.com",
-  messagingSenderId: "3364240751",
-  appId: "1:3364240751:web:c52e7c469f154841433c9a",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: "G-HK453B5FVT"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const firestore = getFirestore(app);
+const firestore = getFirestore(app)
 
 export {firestore}
